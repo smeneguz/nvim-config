f778ebd045d4a90a5bcbeb6e1f7aee88dee95bec
Silvio Meneguzzo <silviomeneguzzo@MacBook-Pro-di-Silvio.local>
2025-01-17 12:09:28 +0000

    feat: add comprehensive git integration
    - Add gitsigns for inline git information
    - Setup fugitive for git commands
    - Configure lazygit integration
    - Add diffview for git diffs
    - Configure git-related keybindings

---
+++ lua/silvio/plugins/diffview.lua
@@ -0,0 +1,15 @@
+return {
+    'sindrets/diffview.nvim',
+    lazy = true,
+    cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+    init = function()
+        vim.opt.fillchars:append { diff = "╱" }
+    end,
+    keys = {
+        { "<leader>gd", "<cmd>DiffviewOpen<cr>", mode = "n", desc = "Open diffview" },
+        { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", mode = "n", desc = "Open diffview current file history" },
+        { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", mode = "n", desc = "Open diffview branch history" },
+        { "<leader>gm", "<cmd>:DiffviewOpen origin/main...HEAD<cr>", mode = "n", desc = "diffview HEAD with origin/main" },
+        { "<leader>gc", "<cmd>DiffviewClose<cr>", mode = "n", desc = "Close diffview" },
+    }
+}
---
+++ lua/silvio/plugins/gitsigns.lua
@@ -0,0 +1,63 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    ft = { "gitcommit", "diff" },
+    event = "BufEnter",
+    init = function()
+        -- load gitsigns only when a git file is opened
+        vim.api.nvim_create_autocmd({ "BufRead" }, {
+            group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+            callback = function()
+                vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
+                if vim.v.shell_error == 0 then
+                    vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
+                    vim.schedule(function()
+                        require("lazy").load { plugins = { "gitsigns.nvim" } }
+                    end)
+                end
+            end,
+        })
+    end,
+    config = function(_, _)
+        require('gitsigns').setup({
+            on_attach = function(bufnr)
+                local gs = require('gitsigns')
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                map('n', ']h', function()
+                    if vim.wo.diff then return ']h' end
+                    vim.schedule(function() gs.next_hunk() end)
+                    return '<Ignore>'
+                end, { expr = true, desc = "Next hunk" })
+
+                map('n', '[h', function()
+                    if vim.wo.diff then return '[h' end
+                    vim.schedule(function() gs.prev_hunk() end)
+                    return '<Ignore>'
+                end, { expr = true, desc = "Previous hunk" })
+
+                -- Actions
+                map('n', '<leader>hs', gs.stage_hunk, { desc = "Stage hunk" })
+                map('n', '<leader>hr', gs.reset_hunk, { desc = "Reset hunk" })
+                map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage buffer" })
+                map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+                map('n', '<leader>hR', gs.reset_buffer, { desc = "Reset buffer" })
+                map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview hunk" })
+                map('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = "Blame line" })
+                map('n', '<leader>gb', gs.toggle_current_line_blame, { desc = "Toggle current line blame" })
+                map('n', '<leader>hd', gs.diffthis, { desc = "Diff this" })
+                map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = "Diff this ~" })
+
+                -- Text object
+                map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+            end
+        })
+    end,
+}
